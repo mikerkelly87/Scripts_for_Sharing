3162c4802f3ddf05dd4b3bf33374ed5285b76b5e
Mike Kelly <mikerkelly87@gmail.com>
2018-12-22 03:39:20 -0600

    Removed all hard coded values from the python script itself

--- interfaces_generate/replace_interfaces.py
+++ interfaces_generate/replace_interfaces.py
@@ -4,61 +4,46 @@ import yaml
 from shutil import copyfile
 import fileinput
 
+# create interfaces object
+with open("interface-list.yml", 'r') as ymlfile:
+    interfaces = yaml.load(ymlfile)
+
+# create addresses object
 with open("addresses.yml", 'r') as ymlfile:
     addresses = yaml.load(ymlfile)
 
-for i in addresses:
-	print(i)
-	print("br-mgmt")
-	print(addresses[i]['br_mgmt_'])
-	print("br-storage")
-	print(addresses[i]['br_storage_'])
-	print("br-storageprod")
-	print(addresses[i]['br_storageprod_'])
-	print("br-vlan")
-	print(addresses[i]['br_vlan_'])
-	print("br-vxlan")
-	print(addresses[i]['br_vxlan_'])
-	print("br-swift")
-	print(addresses[i]['br_swift_'])
-	print("")
+print("")
+print("List of Interfaces")
 
+# print interfaces (for testing)
+for i in interfaces:
+#	print("List of Interfaces")
+	print(i)
+
+print("")
+
+# copy interfaces file template for each server provided in addresses.yml
 for i in addresses:
 	print("Copying File for "+i)
 	copyfile("interfaces.txt", i+"interfaces.txt")
 
 print("")
 
-print("Replacing IP Addressess")
-
+# print all the interfaces (for testing)
 for i in addresses:
-    for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
-        line = line.replace("br_mgmt_",addresses[i]['br_mgmt_'])
-        print line,
-    else:
-    	print
-    for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
-        line = line.replace("br_storage_",addresses[i]['br_storage_'])
-        print line,
-    else:
-    	print
-    for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
-        line = line.replace("br_storageprod_",addresses[i]['br_storageprod_'])
-        print line,
-    else:
-    	print
-    for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
-        line = line.replace("br_vlan_",addresses[i]['br_vlan_'])
-        print line,
-    else:
-    	print
-    for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
-        line = line.replace("br_vxlan_",addresses[i]['br_vxlan_'])
-        print line,
-    else:
-    	print
-    for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
-        line = line.replace("br_swift_",addresses[i]['br_swift_'])
-        print line,
-    else:
-    	print
+	print("")
+	print(i)
+	for n in interfaces:
+            print(n,addresses[i][n])
+
+print("")
+
+print("Replaceing IP Addresses")
+
+# Where the magic happens
+for i in addresses:
+	print("Setting IPs in file for",i)
+	for n in interfaces:
+            for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
+                line = line.replace(n,addresses[i][n])
+                print line,
