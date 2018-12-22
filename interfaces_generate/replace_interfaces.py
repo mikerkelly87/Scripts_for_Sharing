e9e9b4fcd3bfd2a564aa6ce05a41f2480f95119e
Mike Kelly <mikerkelly87@gmail.com>
2018-12-22 01:57:55 -0600

    Adding Extra Interfaces

--- interfaces_generate/replace_interfaces.py
+++ interfaces_generate/replace_interfaces.py
@@ -9,14 +9,18 @@ with open("addresses.yml", 'r') as ymlfile:
 
 for i in addresses:
 	print(i)
-	print("br-host")
-	print(addresses[i]['br_host'])
 	print("br-mgmt")
 	print(addresses[i]['br_mgmt'])
 	print("br-storage")
 	print(addresses[i]['br_storage'])
+	print("br-storageprod")
+	print(addresses[i]['br_storageprod'])
+	print("br-vlan")
+	print(addresses[i]['br_vlan'])
 	print("br-vxlan")
 	print(addresses[i]['br_vxlan'])
+	print("br-swift")
+	print(addresses[i]['br_swift'])
 	print("")
 
 for i in addresses:
@@ -29,11 +33,6 @@ print("Replacing IP Addressess")
 
 for i in addresses:
     for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
-        line = line.replace("br_host",addresses[i]['br_host'])
-        print line,
-    else:
-    	print
-    for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
         line = line.replace("br_mgmt",addresses[i]['br_mgmt'])
         print line,
     else:
@@ -44,7 +43,22 @@ for i in addresses:
     else:
     	print
     for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
+        line = line.replace("br_storageprod",addresses[i]['br_storageprod'])
+        print line,
+    else:
+    	print
+    for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
+        line = line.replace("br_vlan",addresses[i]['br_vlan'])
+        print line,
+    else:
+    	print
+    for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
         line = line.replace("br_vxlan",addresses[i]['br_vxlan'])
+        print line,
+    else:
+    	print
+    for line in fileinput.FileInput(i+"interfaces.txt",inplace=1):
+        line = line.replace("br_swift",addresses[i]['br_swift'])
         print line,
     else:
     	print
