ed40cc15992e60842090d873f788841bc3808564
Mike Kelly <mikerkelly87@gmail.com>
2019-04-28 17:33:41 -0500

    Allowed creating of multipe VMs

--- libvirt_lab_setup/create_vms.sh
+++ libvirt_lab_setup/create_vms.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
-# set variables
-VM_NAME=ubuntu1
-DISK_NAME=$VM_NAME.qcow2
+# Function to set variables based on VM names provided
+set_variables () {
+    echo "Set variables for VM $1"
+    VM_NAME=$1
+    DISK_NAME=$VM_NAME.qcow2
+}
 
-# copy the base disk image
-cp /var/lib/libvirt/images/ubuntu18.04-base.qcow2 /var/lib/libvirt/images/$DISK_NAME
+# Function to copy the base image to be used for the VMs
+copy_image () {
+    echo "Copy disk image for $VM_NAME"
+    #cp /var/lib/libvirt/images/ubuntu18.04-base.qcow2 /var/lib/libvirt/images/$DISK_NAME
+    cp /var/lib/libvirt/images/ubuntu16.04-base.qcow2 /var/lib/libvirt/images/$DISK_NAME
+}
 
-# create vm
-virt-install --name "$VM_NAME" --memory 2048 --vcpus 2 --import --disk "/var/lib/libvirt/images/$DISK_NAME" --network type=direct,source=enp60s0,source_mode=bridge,model=virtio --network default --noautoconsole
+# Function to create the VMs
+create_vms () {
+    echo "Create the VM $VM_NAME"
+    #virt-install --name "$VM_NAME" --memory 2048 --vcpus 2 --import --disk "/var/lib/libvirt/images/$DISK_NAME" --network default --network type=direct,source=enp60s0,source_mode=bridge,model=virtio --os-variant=ubuntu18.04 --noautoconsole
+    virt-install --name "$VM_NAME" --memory 2048 --vcpus 2 --import --disk "/var/lib/libvirt/images/$DISK_NAME" --network default --network type=direct,source=enp60s0,source_mode=bridge,model=virtio --os-variant=ubuntu16.04 --noautoconsole
+}
 
+# Make sure the user who runs this script provides VM names
+if [ "$1" = "" ]; then
+	echo "Please run the script with a list of VM names. EX:"
+	echo "./create_vms vm1 vm2 vm3"
+fi
+
+# Loop through all the VM names that were provided
+while [ "$1" != "" ]; do
+
+    set_variables $1
+    copy_image
+    create_vms
+    
+    # Shift all the parameters down by one
+    shift
+
+done
--- libvirt_lab_setup/list_ips.sh
+++ libvirt_lab_setup/list_ips.sh
@@ -1,10 +1,14 @@
-#/bin/bash
+#!/bin/bash
 
-VM_NAME=ubuntu1
-IP=$(virsh domifaddr $VM_NAME | grep ipv4 | awk '{print $4}' | cut -d'/' -f1)
-LAN_IP=$(ssh $IP "ip a | grep 10.0.0" | awk '{print $2}' | cut -d'/' -f1)
-echo ""
-echo "The INTERNAL IP is $IP"
-echo "The LAN IP is $LAN_IP"
-echo ""
-
+for i in $(virsh list | grep -v Name | awk '{print $2}')
+do
+    VM_NAME=$i
+    LAN_IP=$(ssh $IP "ip a | grep 10.0.0" | awk '{print $2}' | cut -d'/' -f1)
+    IP=$(virsh domifaddr $i | grep ipv4 | awk '{print $4}' | cut -d'/' -f1)
+    LAN_IP=$(ssh -o StrictHostKeyChecking=no $IP "ip a | grep 10.0.0" | awk '{print $2}' | cut -d'/' -f1)
+    echo ""
+    echo "Here are the IPs for $i"
+    echo "Internal IP: $IP"
+    echo "LAN IP: $LAN_IP"
+    echo ""
+done
