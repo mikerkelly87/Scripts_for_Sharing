de338b4a1ae475de1e78ac226595044b76d1c75f
Mike Kelly <mikerkelly87@gmail.com>
2019-04-29 01:22:38 -0500

    Adding distro choice

--- libvirt_lab_setup/create_vms.sh
+++ libvirt_lab_setup/create_vms.sh
@@ -7,17 +7,39 @@ set_variables () {
     DISK_NAME=$VM_NAME.qcow2
 }
 
+prompt_user () {
+    echo ""
+    PS3='Please select your distro: '
+    options=("Ubuntu 16.04" "CentOS 7")
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "Ubuntu 16.04")
+                echo "you chose Ubuntu 16.04"
+                BASE_DISK=ubuntu16.04-base.qcow2
+		echo ""
+                break
+                ;;
+            "CentOS 7")
+                echo "you chose CentOS 7"
+                BASE_DISK=base-centos7.qcow2
+		echo ""
+                break
+                ;;
+            *) echo "invalid option $REPLY";;
+        esac
+    done
+}
+
 # Function to copy the base image to be used for the VMs
 copy_image () {
     echo "Copy disk image for $VM_NAME"
-    #cp /var/lib/libvirt/images/ubuntu18.04-base.qcow2 /var/lib/libvirt/images/$DISK_NAME
-    cp /var/lib/libvirt/images/ubuntu16.04-base.qcow2 /var/lib/libvirt/images/$DISK_NAME
+    cp /var/lib/libvirt/images/$BASE_DISK /var/lib/libvirt/images/$DISK_NAME
 }
 
 # Function to create the VMs
 create_vms () {
     echo "Create the VM $VM_NAME"
-    #virt-install --name "$VM_NAME" --memory 2048 --vcpus 2 --import --disk "/var/lib/libvirt/images/$DISK_NAME" --network default --network type=direct,source=enp60s0,source_mode=bridge,model=virtio --os-variant=ubuntu18.04 --noautoconsole
     virt-install --name "$VM_NAME" --memory 2048 --vcpus 2 --import --disk "/var/lib/libvirt/images/$DISK_NAME" --network default --network type=direct,source=enp60s0,source_mode=bridge,model=virtio --os-variant=ubuntu16.04 --noautoconsole
 }
 
@@ -31,6 +53,7 @@ fi
 while [ "$1" != "" ]; do
 
     set_variables $1
+    prompt_user
     copy_image
     create_vms
     
